a3ece7552b5bf4420c59974229a0e32a3c21274f
makhijakabir <kabirmakhija05@gmail.com>
2021-04-05 13:39:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelectorAll(".list")[2].innerHTML = "Kabir Makhija";
+document.querySelector("button").style.backgroundColor = "Yellow";
+document.querySelector("h1").classList.add("huge");
